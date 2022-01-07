19fd43898ffc11dc8c3d20e1db88c6b7b18c24a6
Connor Hollis <connorhollis3d@gmail.com>
2022-01-07 15:15:12 -0700

    added voters table database

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,15 @@
 -- Deletes tables if they exist
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
+
+CREATE TABLE voters (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
 
 -- Creates parties table
 CREATE TABLE parties (
